65d25b40ffac982a769043dc5d0afac34b7b1527
Chris <hadal.division.manager@gmail.com>
2026-07-23 00:53:48 -0600

    Fix missing BaseClasses using in Firecracker.cs

--- Content/Items/BuffItems/Firecracker.cs
+++ Content/Items/BuffItems/Firecracker.cs
@@ -1,5 +1,6 @@
 using OmoriMod.Content.Buffs.Abstract;
 using OmoriMod.Content.Items.Abstract_Classes;
+using OmoriMod.Content.Items.Abstract_Classes.BaseClasses;
 using OmoriMod.Content.Items.Abstract_Classes.Emotion_Classes;
 using OmoriMod.Content.Items.BuffItems;
 using OmoriMod.Content.Players;
